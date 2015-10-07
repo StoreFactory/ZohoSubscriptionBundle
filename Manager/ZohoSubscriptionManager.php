9fbc99d9c606cdad0397b7c4a67a051aaf4fa5e7
TristanPerchec <tristan.perchec@yproximite.com>
2015-10-07 18:15:31 +0200

    Began the real manager. Coding blind and don't think it will work

---
+++ Manager/ZohoSubscriptionManager.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace ZohoSubscriptionBundle\Manager;
+
+use AddonApi;
+use PlanApi;
+
+class ZohoSubscriptionManager
+{
+    /**
+     * @var AddonApi
+     */
+    protected $addonApi;
+
+    /**
+     * @var bool
+     */
+    protected $cache;
+
+    /**
+     * @var PlanApi
+     */
+    protected $planApi;
+
+    /**
+     * @var Client
+     */
+    protected $redis;
+
+    /**
+     * @var int
+     */
+    protected $ttl;
+
+    /**
+     * @var string
+     */
+    protected $token;
+
+    /**
+     * @var string
+     */
+    protected $organizationId;
+
+    /**
+     * @param Client $redis Redis client
+     * @param bool   $cache Whether the cache is active or not
+     * @param int    $ttl   Time to keep the cache in seconds
+     */
+    public function __construct(PlanApi $planApi, AddonApi $addonApi, $cache = false, Client $redis = null, $ttl = 3600, $token, $organizationId)
+    {
+        $this->planApi = $planApi;
+        $this->addonApi = $addonApi;
+        $this->cache = $cache;
+        $this->redis = $redis;
+        $this->ttl = $ttl;
+        $this->token = $token;
+        $this->organizationId = $organizationId;
+    }
+}
---
+++ README.md
@@ -0,0 +1,22 @@
+# zoho-subscription-bundle
+A bundle using an implementation of Zoho API subscription (zoho-subscription-api)
+
+# Installing the Bundle
+
+The recommended way to install it is through Composer.
+
+### Install Composer
+
+```curl -sS https://getcomposer.org/installer | php```
+
+Next, run the Composer command to install the latest version:
+
+```composer.phar require storefactory/zoho-subscription-bundle```
+
+After installing, you need to require Composer's autoloader:
+
+```require 'vendor/autoload.php';```
+
+You can then later update the Bundle using composer:
+
+```composer.phar update```
