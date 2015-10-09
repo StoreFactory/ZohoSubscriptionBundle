25bce63cb6bd19e8af85d30b1aa4339c778e41ae
Tristan Bessoussa <tristan.bessoussa@gmail.com>
2015-10-09 14:44:33 +0200

    WIP Bundle

---
+++ CompilerPass/DynmaicServiceCacheCompilerPass.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * ZohoSubscription Bundle
+ *
+ * @author Tristan Bessoussa <tristan.bessoussa@gmail.com>
+ */
+
+namespace StoreFactory\ZohoSubscriptionBundle\CompilerPass;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
+
+class DynamicServiceCacheCompilerPass implements CompilerPassInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function process(ContainerBuilder $container)
+    {
+        if ($container->getParameter('my_poseo.api.cache_service_id') !== null) {
+            $container
+                ->getDefinition('zoho.api.manager')
+                ->addArgument(new Reference($container->getParameter('zoho_subscription.cache.service')))
+            ;
+        }
+    }
+}
